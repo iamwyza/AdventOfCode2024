e8512e1fcbb8f5b9a5bbccd5dcad0a68a416a337
Jonathon Wyza <iamwyza@gmail.com>
2024-12-03 15:52:05 -0600

    Day 3.  Much easier.

--- AdventOfCode2024/Day03/Day03.cs
+++ AdventOfCode2024/Day03/Day03.cs
@@ -1,7 +1,5 @@
-﻿using AdventOfCode2024.GridUtilities;
-
-namespace AdventOfCode2024.Day03;
-internal class Day03 : DayBase
+﻿namespace AdventOfCode2024.Day03;
+internal partial class Day03 : DayBase
 {
 
     public Day03()
@@ -9,35 +7,82 @@ internal class Day03 : DayBase
         Ready = true;
     }
 
-    private Grid<sbyte> _map;
+    private string[] _lines;
 
-    [MemberNotNull(nameof(_map))]
+    [MemberNotNull(nameof(_lines))]
     private async Task Init(int part, bool useTestData)
     {
-        _map = new Grid<sbyte>();
 
-        var lines = useTestData ? await GetTestLines(part) : await GetLines();
-
-        foreach (var line in lines)
-        {
-
-        }
-
-        _map.InitMap();
-
-        _map.PrintMap();
+        _lines = useTestData ? await GetTestLines(part) : await GetLines();
     }
 
     public override async Task RunPart1()
     {
         PrintStart(1);
-        await Init(1, true);
+        await Init(1, false);
+        var regex = MulRegex();
+
+        var total = 0;
+        foreach (var line in _lines)
+        {
+            var matches = regex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                var first = Convert.ToInt32(match.Groups[1].Value);
+                var second = Convert.ToInt32(match.Groups[2].Value);
+                total += first * second;
+            }
+        }
+        
+        AnsiConsole.MarkupLineInterpolated($"Total is [green]{total}[/]");
 
     }
 
     public override async Task RunPart2()
     {
         PrintStart(2);
-        await Init(1, true);
+        await Init(2, false);
+        
+        var regex = MulWithConditionalRegex();
+
+        var total = 0;
+        var enabled = true;
+        foreach (var line in _lines)
+        {
+            var matches = regex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                switch (match.Groups[0].Value)
+                {
+                    case "do()":
+                        enabled = true;
+                        break;
+                    case "don't()":
+                        enabled = false;
+                        break;
+                    default:
+                    {
+                        if (enabled)
+                        {
+                            var first = Convert.ToInt32(match.Groups[3].Value);
+                            var second = Convert.ToInt32(match.Groups[4].Value);
+                            total += first * second;    
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+        
+        AnsiConsole.MarkupLineInterpolated($"Total is [green]{total}[/]");
     }
+
+    [GeneratedRegex(@"((mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\)))")]
+    private static partial Regex MulWithConditionalRegex();
+    
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex MulRegex();
 }
